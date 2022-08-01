f251d522d5407191333551c9ad7c400de3cdb204
arfan36 <arfanur36@gmail.com>
2022-08-01 15:55:02 +0600

    17-6 Make conditional dicision...

---
+++ 6_conditional.js
@@ -0,0 +1,22 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+
+if (iphonePrice < myBudget) {
+    console.log('iphone kine futani marbo');
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+// if (chickenPrice <= myMoney) {
+//     console.log('Murgir raan khamu and haddi chibamu');
+// }
+// if (chickenPrice > myMoney) {
+//     console.log('Smash potato and lentiles soup');
+// }
+
+if (chickenPrice < myMoney) {
+    console.log('I will eat Chicken');
+}
+else {
+    console.log("I will eat potato");
+}
