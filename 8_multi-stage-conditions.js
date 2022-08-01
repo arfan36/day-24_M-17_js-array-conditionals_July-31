829c1015bfecf39b3255ab850daf1103a8029227
arfan36 <arfanur36@gmail.com>
2022-08-01 16:32:23 +0600

    17-8 (advanced) Multi stage ...

---
+++ 8_multi-stage-conditions.js
@@ -0,0 +1,17 @@
+var money = 68;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBuscuit = 20;
+
+if (danishPrice < money) {
+    console.log('Dan Da danish khamu');
+}
+else if (butterBread < money) {
+    console.log('Butter bon khamu');
+}
+else if (toastBuscuit < money) {
+    console.log('chubai chubai toast biscuit khamu');
+}
+else {
+    console.log('Khali cha e sasther jonno valo');
+}
