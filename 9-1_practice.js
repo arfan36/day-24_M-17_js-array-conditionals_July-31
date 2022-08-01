2fed0b4017e5c0623def1de63e769bbfe55dafc9
arfan36 <arfanur36@gmail.com>
2022-08-01 18:39:21 +0600

    17-9 Module summary...

---
+++ 9-1_practice.js
@@ -0,0 +1,32 @@
+var myScore = 85;
+var TomScore = 66;
+var JaneScore = 95;
+var PeterScore = 56;
+var JohnScore = 40;
+
+score = JaneScore;
+
+// var name = ['myScore', 'TomScore', 'JaneScore', 'PeterScore', 'JaneScore'];
+// var score = [85, 66, 95, 56, 40];
+
+if (score >= 80) {
+    console.log('Your grade "A"');
+}
+else if (score >= 60) {
+    console.log('Your grace "B"');
+}
+else if (score >= 50) {
+    console.log('Your grace "C"');
+}
+else if (score >= 40) {
+    console.log('Your grace "D"');
+}
+else {
+    console.log('Your grace "f"');
+}
+
+
+// quiz
+// 6.
+var nums = [1, 5, 8];
+console.log(nums.indexOf(0));
---
+++ 9_summary.js
@@ -0,0 +1,22 @@
+var destinations = ['sajek', 'coxsbazar', 'nepal', 'bali', 'SP'];
+
+console.log(destinations[2]);
+destinations[2] = 'thailand';
+var index = destinations.indexOf('bali');
+console.log(index);
+
+destinations.push('bhutan');
+console.log(destinations);
+
+if (destinations[1] === 'sajek') {
+
+}
+else if (destinations[2] === 'bali') {
+
+}
+else if(destinations.length !== 3){
+
+}
+else{
+     
+}
